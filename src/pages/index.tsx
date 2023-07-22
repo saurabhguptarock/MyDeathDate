3aae919aed45e91ad983c97fb994d3b2e47d282e
saurabh-antcreatives <saurabh.gupta@antcreatives.com>
2023-07-23 00:03:09 +0530

    fixed bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,33 +3,38 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const countdown = document.querySelector(".countdown");
-
-    const launchDate = new Date("Aug 28, 2069 12:00:00").getTime();
+    const countdown = document.querySelector(".countdown") || undefined;
+    setTime(countdown);
 
     const intvl = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = launchDate - now;
-
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const sec = Math.floor((distance % (1000 * 60)) / 1000);
-
-      if (!countdown) return;
-
-      countdown.innerHTML = `
-      <div>${days}<span>DAYS</span></div>
-      <div>${("0" + hours).slice(-2)}<span>HOURS</span></div>
-      <div>${("0" + mins).slice(-2)}<span>MINUTES</span></div>
-      <div>${("0" + sec).slice(-2)}<span>SECONDS</span></div>
-      `;
+      setTime(countdown);
     }, 1000);
 
     return () => clearInterval(intvl);
   }, []);
+
+  const setTime = (countdown?: Element) => {
+    const launchDate = new Date("Aug 28, 2069 12:00:00").getTime();
+
+    const now = new Date().getTime();
+    const distance = launchDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const sec = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (!countdown) return;
+
+    countdown.innerHTML = `
+    <div>${days}<span>DAYS</span></div>
+    <div>${("0" + hours).slice(-2)}<span>HOURS</span></div>
+    <div>${("0" + mins).slice(-2)}<span>MINUTES</span></div>
+    <div>${("0" + sec).slice(-2)}<span>SECONDS</span></div>
+    `;
+  };
 
   return (
     <div>
